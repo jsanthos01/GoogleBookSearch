50dc4f9f60e22a4a49fb97457162db6cb6f6b45d
jsanthos01 <chinu.s2001@gmail.com>
2020-04-14 14:31:36 -0400

    hello

--- db/orm.mongoose.js
+++ db/orm.mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/googlebooks`, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`, {useNewUrlParser: true});
 const db = require( './models' );
 
 
