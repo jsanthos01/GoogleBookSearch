b296e8d654539672eed7d9f893b3d50faea89795
jsanthos01 <chinu.s2001@gmail.com>
2020-04-14 13:38:53 -0400

    heroku

---
+++ build/precache-manifest.9724e540f1aeb44e137e115f1f284e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac53a56a525f4e6046222d10da1cdfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99529089357360a7a901",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ccd4a13b9e044f7c20d7",
+    "url": "/static/js/2.6f130c41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f130c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99529089357360a7a901",
+    "url": "/static/js/main.724165d0.chunk.js"
+  },
+  {
+    "revision": "d86d7721ed60523e60a7",
+    "url": "/static/js/runtime-main.4b911b76.js"
+  }
+]);
